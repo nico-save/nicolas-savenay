a4c1b581e00316ce86e69dae0097ad37f19bf5a6
Nicolas Savenay <nicolas.savenay@gmail.com>
2022-03-01 23:26:26 +0100

    initial commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,20 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
+const detailsAfpaBtn = document.querySelector('.detail-afpa-btn');
+const detailsProshopBtn = document.querySelector('.detail-proshop-btn');
+const detailsNatoursBtn = document.querySelector('.detail-natours-btn');
+const details = document.querySelector('.details');
+const detailsItems = document.querySelectorAll('.details-item');
+
+const modalAfpa = document.getElementById('modal-afpa');
+const modalProshop = document.getElementById('modal-proshop');
+const modalNatours = document.getElementById('modal-natours');
+const modals = document.getElementsByClassName('modal');
+const modalsScreen = [...modals];
+const spans = document.getElementsByClassName('close');
+const closeBtns = [...spans];
+
 // Set Initial State Of Menu
 let showMenu = false;
 
@@ -30,4 +44,33 @@ const toggleMenu = () => {
   }
 };
 
+const toggleDetailAfpa = () => {
+  modalAfpa.classList.add('show');
+};
+
+const toggleDetailProshop = () => {
+  modalProshop.classList.add('show');
+};
+
+const toggleDetailNatours = () => {
+  modalNatours.classList.add('show');
+};
+const closeModal = () => {
+  modalAfpa.classList.remove('show');
+  modalProshop.classList.remove('show');
+  modalNatours.classList.remove('show');
+};
+
 menuBtn.addEventListener('click', toggleMenu);
+
+detailsAfpaBtn.addEventListener('click', toggleDetailAfpa);
+detailsProshopBtn.addEventListener('click', toggleDetailProshop);
+detailsNatoursBtn.addEventListener('click', toggleDetailNatours);
+closeBtns.forEach((btn) => btn.addEventListener('click', closeModal));
+window.onclick = (event) => {
+  modalsScreen.forEach((modal) => {
+    if (event.target == modal) {
+      modal.classList.remove('show');
+    }
+  });
+};
